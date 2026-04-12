f2b67f6cb4310b9908d2efa08d102e98c73132e8
Billuc <luc.billaud.pro@gmail.com>
2026-04-12 16:06:17 +0200

    feat: added License

--- lib/island.ts
+++ lib/island.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2026 Luc BILLAUD
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ **/
 import type { ComponentType } from "preact";
 
 const ISLAND_PATH = Symbol("island-path");
@@ -8,7 +23,7 @@ export type IslandComponent<P = {}> = ComponentType<P> & {
 
 /**
  * Defines an island component with its module path for server-side rendering.
- * 
+ *
  * @template T - The props type for the component
  * @param component - The Preact component to define as an island
  * @param modulePath - The module path to the component for SSR hydration
